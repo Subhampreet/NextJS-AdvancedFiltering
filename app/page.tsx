784535c25a2dfe887c71c54f5911b58f69d0f716
Subhampreet <subhampreet18@gmail.com>
2024-03-31 18:43:14 +0530

    Filter Icon UI Added

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/utils';
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, Filter } from 'lucide-react';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -53,6 +53,10 @@ export default function Home() {
               ))}
             </DropdownMenuContent>
           </DropdownMenu>
+
+          <button className='-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden'>
+            <Filter className='h-5 w-5' />
+          </button>
         </div>
       </div>
     </main>
