7774cbbd43621ecf6b8f8d593f2c6bdfda651009
Subhampreet <subhampreet18@gmail.com>
2024-04-01 19:28:19 +0530

    Product Component Build 1.0

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,8 @@ import Image from 'next/image';
 import { useState } from 'react';
 import axios from "axios"
 import { QueryResult } from '@upstash/vector';
-import { Product } from '@/db';
+import type { Product as TProduct} from '@/db';
+import Product from '@/components/product/Product';
 
 const SORT_OPTIONS = [
   { name: 'None', value: 'none' },
@@ -28,7 +29,7 @@ export default function Home() {
   const {data: products} = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      const {data} = await axios.post<QueryResult<Product>[]>(
+      const {data} = await axios.post<QueryResult<TProduct>[]>(
         'http://localhost:3000/api/products', {
           filter: {
             sort: filter.sort,
@@ -38,6 +39,8 @@ export default function Home() {
       return data
     }
   })
+
+  console.log(products);
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -77,6 +80,21 @@ export default function Home() {
           </button>
         </div>
       </div>
+
+      <section className='pb-24 pt-6'>
+        <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
+
+          <div></div>
+
+          <ul className='lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
+            {
+              products?.map((product) => (
+                <Product product={product.metadata!} />
+              ))
+            }
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
