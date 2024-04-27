ad0a5169fef22820df8770bbb30b86fe6ba435a3
Subhampreet <subhampreet18@gmail.com>
2024-04-27 22:41:34 +0530

    applyArrayFilter Fuction declared

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ import Product from '@/components/product/Product';
 import ProductSkeleton from '@/components/product/ProductSkeleton';
 import { Accordion, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { AccordionContent } from '@radix-ui/react-accordion';
+import { ProductState } from '@/lib/validators/product-validator';
 
 const SORT_OPTIONS = [
   { name: 'None', value: 'none' },
@@ -69,6 +70,28 @@ export default function Home() {
 
   // console.log(products);
 
+  const applyArrayFilter = ({
+    category, value
+  }: {
+    category: keyof Omit<typeof filter, "price" | "sort">
+    value: string
+  }) => {
+    const isFilterApplied = filter[category].includes(value as never)
+
+    if(isFilterApplied){
+      setFilter((prev) => ({
+        ...prev,
+        [category]:  prev[category].filter((v) => v !== value)
+      }))
+    }
+    else{
+      setFilter((prev) => ({
+        ...prev,
+        [category]: [...prev[category], value]
+      }))
+    }
+  }
+ 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -131,7 +154,18 @@ export default function Home() {
                     {
                       COLORS_FILTERS.options.map((option, optionIdx) => (
                         <li key={option.value} className='flex items-center'>
-                          <input type="checkbox" id={`color-${optionIdx}`} className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' />
+                          <input 
+                            type="checkbox" 
+                            id={`color-${optionIdx}`} 
+                            onChange={() => {
+                              applyArrayFilter({
+                                category: "color",
+                                value: option.value
+                              })
+                            }}
+                            checked={filter.color.includes(option.value)}
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' 
+                          />
                           <label htmlFor={`color-${optionIdx}`} className='ml-3 text-sm text-gray-600'>{option.label}</label>
                         </li>
                       ))
