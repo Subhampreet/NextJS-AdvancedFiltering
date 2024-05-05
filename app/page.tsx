db2b29b6bdfc3d3625f621f2a3bf0cc83009ef8f
Subhampreet <subhampreet18@gmail.com>
2024-05-05 21:40:21 +0530

    Size Filtering Added

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,16 @@ const COLORS_FILTERS = {
   ] as const,
 }
 
+const SIZE_FILTERS = {
+  id: "size",
+  name: "Size",
+  options : [
+    {value : 'S', label : 'S'},
+    {value : 'M', label : 'M'},
+    {value : 'L', label : 'L'}
+  ] as const,
+}
+
 const SUBCATEGORIES = [
   { name : 'T-Shirts', selected: true, href: '#'},
   { name : 'Hoodies', selected: false, href: '#'},
@@ -144,7 +154,7 @@ export default function Home() {
             </ul>
 
             <Accordion type="multiple" className="animate-none">
-              {/* Color */}
+              {/* Color Filter */}
               <AccordionItem value="color">
                 <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500" >
                   <span className='font-medium text-gray-900'>Color</span>
@@ -167,6 +177,36 @@ export default function Home() {
                             className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' 
                           />
                           <label htmlFor={`color-${optionIdx}`} className='ml-3 text-sm text-gray-600'>{option.label}</label>
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+
+              {/*-------------------- Size Filter ------------ */}
+              <AccordionItem value="size">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500" >
+                  <span className='font-medium text-gray-900'>Size</span>
+                </AccordionTrigger>
+                <AccordionContent className='pt-2 animate-none'>
+                  <ul className='space-y-4 pb-4'>
+                    {
+                      SIZE_FILTERS.options.map((option, optionIdx) => (
+                        <li key={option.value} className='flex items-center'>
+                          <input 
+                            type="checkbox" 
+                            id={`size-${optionIdx}`} 
+                            onChange={() => {
+                              applyArrayFilter({
+                                category: "size",
+                                value: option.value
+                              })
+                            }}
+                            checked={filter.size.includes(option.value)}
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' 
+                          />
+                          <label htmlFor={`size-${optionIdx}`} className='ml-3 text-sm text-gray-600'>{option.label}</label>
                         </li>
                       ))
                     }
